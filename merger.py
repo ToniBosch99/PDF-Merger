490b897e1bd249a35f768fc13f67e7f4140d1715
LAPTOP-NTUAN\Toni <toniboschllonch@outlook.com>
2023-02-23 12:43:35 +0100

    First version Complete

--- merger.py
+++ merger.py
@@ -1,16 +1,14 @@
 from PyPDF2 import PdfMerger
 from searcher import open_window
 
-pdf_list, out_folder, out_name = open_window()
-print(pdf_list, out_folder, out_name)
+pdf_list, out_path = open_window()
+print(pdf_list, out_path)
 
-# pdfs = [r"C:\Users\Toni\OneDrive\Escriptori\ETSEIB\MUEI\Q3\Tecnologia Energètica\Topic_2.pdf",
-#  r"C:\Users\Toni\OneDrive\Escriptori\ETSEIB\MUEI\Q3\Tecnologia Energètica\Topic_3.pdf"]
 
-# merger = PdfMerger()
+merger = PdfMerger()
 
-# for pdf in pdfs:
-#     merger.append(pdf)
+for pdf in pdf_list:
+    merger.append(pdf)
 
-# merger.write(r"C:\Users\Toni\OneDrive\Escriptori\ETSEIB\MUEI\Q3\Tecnologia Energètica\Merged.pdf")
-# merger.close()
+merger.write(out_path)
+merger.close()
